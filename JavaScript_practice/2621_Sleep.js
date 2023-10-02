ea4b3189be5facaaf91085724871bd49eb9c64cc
davidteng95 <davidteng95@gmail.com>
2023-10-02 16:09:06 -0600

    2621.Sleep JS practice

---
+++ JavaScript_practice/2621_Sleep.js
@@ -0,0 +1,44 @@
+/*Example 1:
+
+Input: millis = 100
+Output: 100
+Explanation: It should return a promise that resolves after 100ms.
+let t = Date.now();
+sleep(100).then(() => {
+  console.log(Date.now() - t); // 100
+});
+Example 2:
+
+Input: millis = 200
+Output: 200
+Explanation: It should return a promise that resolves after 200ms.
+ 
+
+Constraints:
+
+1 <= millis <= 1000
+*/
+
+// Solution
+
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(millis);
+    }, millis);
+  });
+}
+
+let t = Date.now();
+sleep(100).then(() => {
+  console.log(Date.now() - t);
+});
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
