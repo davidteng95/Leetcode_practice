b9b8134c988bf98a41825041b07a31fad012150f
davidteng95 <davidteng95@gmail.com>
2023-10-03 17:43:41 -0600

    include question

--- JavaScript_practice/2621_Sleep.js
+++ JavaScript_practice/2621_Sleep.js
@@ -1,4 +1,7 @@
-/*Example 1:
+/*
+Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
+
+Example 1:
 
 Input: millis = 100
 Output: 100
