3e6a4ce29057c419e7b035c1f1a5386dbf80a180
davidteng95 <davidteng95@gmail.com>
2023-11-02 09:58:01 -0600

    add method 2

--- JavaScript_practice/344_ReverseString.js
+++ JavaScript_practice/344_ReverseString.js
@@ -27,5 +27,21 @@ s[i] is a printable ascii character.
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-  return s.reverse();
+  // Method 1
+  // return s.reverse();
+
+  // Method 2
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left <= right) {
+    const temp = s[left];
+    s[left] = s[right];
+    s[right] = temp;
+
+    left++;
+    right--;
+  }
+
+  return s;
 };
