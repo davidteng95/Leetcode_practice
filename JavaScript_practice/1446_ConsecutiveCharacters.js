baa11b4caaba512fe55caedde75cb4a0f7228a82
davidteng95 <davidteng95@gmail.com>
2024-10-29 20:15:45 -0600

    1446.Consecutive Characters JS practice

---
+++ JavaScript_practice/1446_ConsecutiveCharacters.js
@@ -0,0 +1,46 @@
+/*
+The power of the string is the maximum length of a non-empty substring that contains only one unique character.
+
+Given a string s, return the power of s.
+
+ 
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 2
+Explanation: The substring "ee" is of length 2 with the character 'e' only.
+Example 2:
+
+Input: s = "abbcccddddeeeeedcba"
+Output: 5
+Explanation: The substring "eeeee" is of length 5 with the character 'e' only.
+ 
+
+Constraints:
+
+1 <= s.length <= 500
+s consists of only lowercase English letters.
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxPower = function(s) {
+  let maxCount = 1;
+  let count = 1;
+  for (let i = 0; i < s.length - 1; i++) {
+      if (s[i] === s[i + 1]) {
+          count++;
+      } else if (s[i] !== s[i + 1]) {
+          count = 1;
+      }
+
+      if (count > maxCount) {
+          maxCount = count;
+      }
+  }
+
+  return maxCount;
+};
