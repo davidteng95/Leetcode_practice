3cc18c9293b8946870f54883c369f4be8e5012c1
davidteng95 <davidteng95@gmail.com>
2025-01-29 19:38:11 -0700

    1207.Unique Number of Occurrences JS practice

---
+++ JavaScript_practice/1207_UniqueNumberOfOccurrences.js
@@ -0,0 +1,50 @@
+/*
+Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+ 
+
+Example 1:
+
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+Example 2:
+
+Input: arr = [1,2]
+Output: false
+Example 3:
+
+Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+Output: true
+ 
+
+Constraints:
+
+1 <= arr.length <= 1000
+-1000 <= arr[i] <= 1000
+*/
+
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function(arr) {
+  let count = {};
+  for (let i = 0; i < arr.length; i++) {
+      count[arr[i]] = (count[arr[i]] || 0) + 1;
+  }
+
+  let store = [];
+  for (let occur in count) {
+      store.push(count[occur]);
+  }
+
+  store.sort((a, b) => a - b);
+  for (let j = 0; j < store.length - 1; j++) {
+      if (store[j] === store[j + 1]) {
+          return false;
+      }
+  }
+
+  return true;
+};
